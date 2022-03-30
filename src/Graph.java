dfbd3a39f454efee23b2103abda126b26909199f
daniellebuggle <99493192+daniellebuggle@users.noreply.github.com>
2022-03-30 16:53:49 +0100

    Created Method to read in stop_times.txt and create edges

---
+++ src/Graph.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Graph {
+    LinkedList<Edge> listOfEdges;
+
+    public Graph(LinkedList<Edge> listOfEdges) {
+        this.listOfEdges = listOfEdges;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args){
         readFileTransfers("transfers.txt");
-        //readFileStopTimes("stop_times.txt");
+        readFileStopTimes("stop_times.txt");
     }
 
     public static void readFileTransfers(String filename){
@@ -34,6 +34,34 @@ public class Main {
                     edges.add(edge);
                 }
             }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFileStopTimes(String filename){
+        try {
+            if (filename == null) {
+                return;
+            }
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            int count = 0;
+            myReader.nextLine(); // skip the first line
+            String[] lineOne = myReader.nextLine().split(",");
+            while (myReader.hasNextLine()) {
+                String[] lineTwo = myReader.nextLine().split(",");
+                if(Objects.equals(lineOne[0], lineTwo[0])) {
+                    Edge edge = new Edge(Integer.parseInt(lineOne[3]), Integer.parseInt(lineTwo[3]),1);
+                    edges.add(edge);
+                }
+                lineOne = lineTwo;
+            }
+            Edge edge = edges.getLast();
+            System.out.println("Vertice One: " + edge.getVerticeOne() + "\nVertice Two: " + edge.getVerticeTwo());
+            System.out.println("Number of Edges: " + edges.size());
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
